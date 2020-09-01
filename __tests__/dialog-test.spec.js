dffa0e011cf7d230226ca51a7c175c8d20d0fd5a
Tiago Umemura <umemuratiago@gmail.com>
2020-09-01 20:51:53 -0300

    Add button click function and verify url

--- __tests__/dialog-test.spec.js
+++ __tests__/dialog-test.spec.js
@@ -1,3 +1,13 @@
+const clickDivByText = async (page, text) => {
+  const linkHandlers = await page.$x(`//div[contains(text(), ${text})]`);
+
+  if (linkHandlers.length > 0) {
+    await linkHandlers[0].click();
+  } else {
+    throw new Error(`Div not found: ${text}`);
+  }
+};
+
 describe("Dialog e2e", () => {
   beforeAll(async () => {
     await page.goto("https://play.google.com/");
@@ -9,8 +19,11 @@ describe("Dialog e2e", () => {
     await page.waitForFunction(
       'document.querySelector("body").innerText.includes("PEPapp - PepsiCo")'
     );
-    await page.click(
+    await clickDivByText(page, "PEPapp");
+    await page.waitForFunction(
       'document.querySelector("body").innerText.includes("PEPapp - PepsiCo")'
     );
+    expect(page.url()).toContain("br.com.eusoupepsico");
+    await page.waitForXPath('//div[@jsname="xBmnf"]', { visible: false });
   });
 });
